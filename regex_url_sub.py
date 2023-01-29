2f9a78843db75a1c6562d04eb10cc1954dcdf7e1
ChristineLwin <christine20130101@gmail.com>
2023-01-29 08:46:30 -0800

    update file

--- regex_url_sub.py
+++ regex_url_sub.py
@@ -9,11 +9,13 @@ Replace all url addresses with <url> tags in the original text string
 
 df = pd.read_csv("./url_data.csv")
 
-url_ptn = r'\bhttps?://\S+\b'
+url_ptn1 = r'\bhttps?://\S+\b'
+# option 2 for url start with www 
+url_ptn2 =  r"((http://)[^ ]*|(https://)[^ ]*|( www\.)[^ ]*)"
 
 for idx,row in df.iterrows():
-    match = re.findall(url_ptn,row['text'])
-    res = re.sub(url_ptn,'<url>',row['text'])
+    match = re.findall(url_ptn1,row['text'])
+    res = re.sub(url_ptn1,'<url>',row['text'])
     df.loc[idx,'processed text'] = res
     if len(match)>0:
         df.loc[idx,'website'] = " ".join(match)
